f48edb209dab7b4a494fd2e04034443596c6afc0
Matthew Diep <MatthewDiep514@gmail.com>
2019-10-20 23:12:54 -0700

    first commit

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -0,0 +1,61 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class StockManagementTests {
+
+	ExternalISBNDataService testWebService;
+	StockManager stockManager;
+	ExternalISBNDataService testDatabaseService;
+	
+	@Before
+	public void setup() {
+		testWebService = mock(ExternalISBNDataService.class);
+		stockManager = new StockManager();
+		stockManager.setWebService(testWebService);
+		testDatabaseService = mock(ExternalISBNDataService.class);
+		stockManager.setDatabaseService(testDatabaseService);
+	}
+		
+	@Test
+	public void testCanGetACorrectLocatorCode() {
+
+		when(testWebService.lookup(anyString())).thenReturn(new Book("0140177396", "Of Mice And Men", "J. Steinbeck"));
+		when(testDatabaseService.lookup(anyString())).thenReturn(null);
+		
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		assertEquals("7396J4", locatorCode);
+	}
+	
+	@Test
+	public void databaseIsUsedIfDataIsPresent() {
+		
+		when(testDatabaseService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
+		
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		
+		verify(testDatabaseService, times(1)).lookup("0140177396");
+		verify(testWebService, times(0)).lookup(anyString());
+	}
+
+	@Test
+	public void webserviceIsUsedIfDataIsNotPresentInDatabase() {
+		
+		when(testDatabaseService.lookup("0140177396")).thenReturn(null);
+		when(testWebService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
+		
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		
+		verify(testDatabaseService, times(1)).lookup("0140177396");
+		verify(testWebService, times(1)).lookup("0140177396");
+		//atLeast(), atMost(), never(), time(1), time(0)
+		//anyString(), any(Book.class), any...
+	}
+}
